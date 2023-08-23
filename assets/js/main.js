000c4be000b574974c89bb0c6ce8d1895c3f5dff
Lakhan Bichukale <lakhanb0508@gmail.com>
2023-08-23 23:57:01 +0530

    product section created

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,83 @@
 /*=============== IMAGE GALLERY ===============*/
 
 /*=============== SWIPER CATEGORIES ===============*/
+var swiperCategories = new Swiper(".categories-container", {
+   spaceBetween: 24,
+   loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 5,
+          spaceBetween: 24,
+        },
+      },
+    });
+  
 /*=============== SWIPER PRODUCTS ===============*/
+var swiperProducts = new Swiper(".new-container", {
+  spaceBetween: 24,
+  loop:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+   slidesPerView: 4,
+   spaceBetween: 10,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
 
+   breakpoints: {
+       640: {
+         slidesPerView: 2,
+         spaceBetween: 20,
+       },
+       768: {
+         slidesPerView: 4,
+         spaceBetween: 40,
+       },
+       1400: {
+         slidesPerView: 4,
+         spaceBetween: 24,
+       },
+     },
+   });
 /*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+tabContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) =>{
+  tab.addEventListener('click', () =>{
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active-tab');
+    });
+
+    target.classList.add('active-tab');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('active-tab');
+    });
+
+    tab.classList.add('active-tab');
+  })
+})
